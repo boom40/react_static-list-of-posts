e27b848db62f244716b6136f9f4e29fefffc725c
boom40 <kickgames675@gmail.com>
2019-12-05 17:27:44 +0200

    Updates

---
+++ precache-manifest.de498e3a1e3a365023ba33f0f7658f09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805550c49c943ef423a9ba30b6d7f961",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "95e071f5dc6b4755b9de",
+    "url": "/react_static-list-of-posts/static/css/main.f04e3a54.chunk.css"
+  },
+  {
+    "revision": "d1bf606ea68b30240b50",
+    "url": "/react_static-list-of-posts/static/js/2.a7975bab.chunk.js"
+  },
+  {
+    "revision": "95e071f5dc6b4755b9de",
+    "url": "/react_static-list-of-posts/static/js/main.1955f26f.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
